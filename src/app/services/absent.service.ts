10c78788680cbc7a6342ce0706ca95dbece82816
DarshikaNemani <dk5673@srmist.edu.in>
2025-07-18 00:09:48 +0530

    Ref: Api get post Absent

--- src/app/services/absent.service.ts
+++ src/app/services/absent.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable } from 'rxjs';
+import { PostAbsentModel } from './models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbsentService {
 
-  constructor() { }
+  constructor(private readonly httpService: HttpService) { }
+
+  postAbsent(absentData: PostAbsentModel): Observable<any> {
+    return this.httpService.postApi('/api/entries/absents/', absentData);
+  }
+
+  getAbsents(): Observable<any> {
+    return this.httpService.getApi('/api/entries/absents/');
+  }
 }
--- src/app/services/models.ts
+++ src/app/services/models.ts
@@ -10,3 +10,40 @@ export interface PostEntryModel {
   type: 'Cash' | 'Wholesale';
   partyName: string;
 }
+
+export interface EntryModel {
+  _id: string;
+  amount: number;
+  date: string;
+  type: 'Cash' | 'Wholesale';
+  partyName: string;
+  __v: number;
+}
+
+export interface EntrySummary {
+  totalCash: number;
+  totalWholesale: number;
+  totalEntries: number;
+  entries?: EntryModel[];
+}
+
+export interface DayEntriesResponse extends EntrySummary {
+  date: string;
+}
+
+export interface MonthEntriesResponse extends EntrySummary {
+  month: number;
+  year: number;
+}
+
+export interface OverallEntriesResponse extends EntrySummary {
+  dateRange?: {
+    from: string;
+    to: string;
+  };
+}
+
+export interface PostAbsentModel {
+  name: string;
+  date: string;
+}

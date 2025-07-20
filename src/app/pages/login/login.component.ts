f8b651a5b8f87920ad7fc670f32c829713ee26e0
DarshikaNemani <dk5673@srmist.edu.in>
2025-07-20 23:36:55 +0530

    Add: Login api

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { FooterComponent } from "../../components/footer/footer.component";
+import { FormsModule } from '@angular/forms';
+import { AdminService } from '../../services/admin.service';
 
 @Component({
   selector: 'app-login',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, FormsModule],
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  password: string = '';
 
+  constructor(private readonly adminService: AdminService) {}
+
+  adminLogin() {
+    if (!this.password) {
+      console.error('Please enter password');
+      return;
+    }
+
+    this.adminService.adminLogin(this.password).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        this.password = '';
+      },
+      error: (error) => {
+        console.error('Login failed:', error);
+      }
+    });
+  }
 }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpService } from './http.service';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  
+  private isLoggedInSubject = new BehaviorSubject<boolean>(false);
+  public isLoggedIn$ = this.isLoggedInSubject.asObservable();
 
-  constructor() { }
+  constructor(private readonly httpService: HttpService) { }
+
+  adminLogin(password: string): Observable<any> {
+    return this.httpService.postApi('/api/admin/login', { password }).pipe(
+      tap((response) => {
+        console.log('Admin login successful:', response);
+        this.isLoggedInSubject.next(true);
+      })
+    );
+  }
+
+  logout() {
+    this.isLoggedInSubject.next(false);
+  }
+
+  get isLoggedIn(): boolean {
+    return this.isLoggedInSubject.value;
+  }
 }
